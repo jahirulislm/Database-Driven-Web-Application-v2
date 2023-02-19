d1af3605aa7982bb362c21ef04cc49b7d34a619e
Jahirul Islam <86736791+jahirulislm@users.noreply.github.com>
2023-02-19 16:12:37 +0000

    secrets

--- database.py
+++ database.py
@@ -1,11 +1,33 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+import os
 
-
+db_connect_string = os.environ("DB_CONNECTION_STRING")
 
 # connection to the database
-engine = create_engine("mysql+pymysql://dvkmh0ivca6aejtwkopd:pscale_pw_y1FzH5npfX0npyjvUsFHVjhmDqa2Seo89gLBIVifk63@ap-southeast.connect.psdb.cloud/webapplication?charset=utf8mb4")
-
+engine = create_engine(
+  db_connect_string,
+  connect_args={"ssl": {
+    "ca": "//C:/Users/jahirul_islam/Downloads"
+  }})
 
 with engine.connect() as conn:
   result = conn.execute(text("select * from jobs"))
   print(result.all())
+
+# from sqlalchemy import create_engine, text
+# # import os
+
+# # db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+# engine = create_engine(
+#   db_connect_string,
+#   connect_args={
+#     "ssl": {
+#       "ssl_ca": ""
+#     }
+#   })
+
+# def load_jobs_from_db():
+#   with engine.connect() as conn:
+#     result = conn.execute(text("select * from jobs"))
+#     print(result.all())
